9006c828ebe558a34203ba519e8448fcf38763a3
Luong Tuan Anh <36061508+n0xgg04@users.noreply.github.com>
2022-12-19 15:33:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // ==UserScript==
-// @author      n0xgg04 - Lương Tuấn Anh
-// @name        Auto đăng ký tín chỉ
-// @version     1.0.0
-// @include     *
+// @name         n0x - QLDT-DKTC
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Auto đăng ký tín chỉ PTIT
+// @author       n0xgg04 - Luong Tuan Anh
 // @updateURL https://raw.githubusercontent.com/n0xgg04/PTIT_Auto_DangKyTin/main/script.js
-// @require http://code.jquery.com/jquery-2.1.0.min.js
-// @run-at      document-start
-// @license MIT
+// @match        https://qldt.ptit.edu.vn/Default.aspx?page=dkmonhoc
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=edu.vn
+// @grant        none
 // ==/UserScript==
 
-console.log("Injection by @n0xgg04")
-$('div#divfilters').after(`</hr>
+(function () {
+    'use strict';
+
+    console.log("Injection by @n0xgg04")
+    $('div#divfilters').after(`</hr>
 <div class="n0xgg04" style="
 margin-left: 20px;
 margin-bottom: 30px;
@@ -71,132 +75,134 @@ margin-bottom: 30px;
 
                        `);
 
-var Danhsach = [
-    {
-        "name": "Giáo dục thể chất",
-        "tableId": "BAS1107",
-        "key": "22",
-    },
-    {
-        "name": "Kinh tế chính trị",
-        "tableId": "BAS1151",
-        "key": "04",
-    },
-    {
-        "name": "Tin học cơ sở 2",
-        "tableId": "INT1155",
-        "key": "15"
-    }
-]
-
-$('#n0x_result').hide()
-
-$('#autopick').click(() => {
-    $('#n0x_result').show()
-    var dss = JSON.parse($('#dataR').val())
-    console.log(dss)
-    RunAuto(dss);
-    $("#autopick").val("Đang đăng ký...")
-});
-
-const removeNth = (arr, n) => {
-   for(let i = n-1; i < arr.length; i += n){
-      arr.splice(i, 1);
-   };
-};
-
-
-const lamMoi = () => {
- var arr =  JSON.parse($('#dataR').val())
- console.log(arr)
-    let i=1;
-    $("#monHoc > tbody").html("");
-    arr.forEach((item) => {
-        $('#monHoc').append(`<tr><td>${i}</td><td>${item.name}</td><td>${item.tableId}</td><td>${item.key}</td></tr>`);
-        i++;
-    })
-}
-
-
-
-
-$('#refresh').click(()=>{
-    lamMoi()
-});
-
-
-let delayTime = $("#delayTime").val()
-
-const switchTo = (maMonHoc) => {
-    var selector = document.getElementById("selectMonHoc").value = maMonHoc
-    selectMonHoc_changed()
-    return;
-}
-
-const writeLog = (content) => {
-    var old = $('#n0x_result').html()
-    $('#n0x_result').html(`${old}${content}`);
-}
-
-const Dangky = (idMon, tenMon = "") => {
-    //   var selector = document.getElementById(idMon)
-    if (document.getElementById(idMon) == null) return 0;
-    if (document.getElementById(idMon).disabled) {
-        console.log(`%cĐăng ký môn ${tenMon} (${idMon}) thất bại do hết slot!\n`, 'color: red;');
-        writeLog(`<p style="color:red;margin-top:0;margin-bottom:0;">Đăng ký môn ${tenMon} (${idMon}) thất bại do hết slot!</p>`);
-        return 1;
-    }
-
-    if (document.getElementById(idMon).checked) {
-        console.log(`%cBạn đã đăng ký ${tenMon} (${idMon}) trước đó!\n`, 'color: red;');
-        writeLog(`<p style="color:red;margin-top:0;margin-bottom:0;">Bạn đã đăng ký ${tenMon} (${idMon}) trước đó!</p>`);
-        return 1;
-    }
-
-    document.getElementById(idMon).checked = true
-    toDKSelectedChange(document.getElementById(idMon))
-    console.log(`%cĐã đăng ký ${tenMon} (${idMon}) thành công`, 'color: green;');
-    writeLog(`<p style="color:green;margin-top:0;margin-bottom:0;">Bạn đã đăng ký ${tenMon} (${idMon}) thành công!</p>`);
-    return 1;
-}
-
-async function Dangkymon(thongtin) {
-    await switchTo(thongtin.tableId);
-    await DelayTime(2000)
-    var failedCount = 0
-    setTimeout(() => {
-        while (Dangky(`chk_${thongtin.tableId}${thongtin.key}    `, thongtin.name) == 0 && failedCount < 10) {
-            // DelayTime()
-            failedCount++;
-            console.error('%cFailed! \n', 'color: black;');
+    var Danhsach = [
+        {
+            "name": "Giáo dục thể chất",
+            "tableId": "BAS1107",
+            "key": "22",
+        },
+        {
+            "name": "Kinh tế chính trị",
+            "tableId": "BAS1151",
+            "key": "04",
+        },
+        {
+            "name": "Tin học cơ sở 2",
+            "tableId": "INT1155",
+            "key": "15"
         }
-    }, 1000)
-}
+    ]
+
+    $('#n0x_result').hide()
+
+    $('#autopick').click(() => {
+        $('#n0x_result').show()
+        var dss = JSON.parse($('#dataR').val())
+        console.log(dss)
+        RunAuto(dss);
+        $("#autopick").val("Đang đăng ký...")
+    });
+
+    const removeNth = (arr, n) => {
+        for (let i = n - 1; i < arr.length; i += n) {
+            arr.splice(i, 1);
+        };
+    };
+
+
+    const lamMoi = () => {
+        var arr = JSON.parse($('#dataR').val())
+        console.log(arr)
+        let i = 1;
+        $("#monHoc > tbody").html("");
+        arr.forEach((item) => {
+            $('#monHoc').append(`<tr><td>${i}</td><td>${item.name}</td><td>${item.tableId}</td><td>${item.key}</td></tr>`);
+            i++;
+        })
+    }
 
 
 
 
+    $('#refresh').click(() => {
+        lamMoi()
+    });
 
 
-const DelayTime = (sec) => {
-    return new Promise((res) => {
+    let delayTime = $("#delayTime").val()
+
+    const switchTo = (maMonHoc) => {
+        var selector = document.getElementById("selectMonHoc").value = maMonHoc
+        selectMonHoc_changed()
+        return;
+    }
+
+    const writeLog = (content) => {
+        var old = $('#n0x_result').html()
+        $('#n0x_result').html(`${old}${content}`);
+    }
+
+    const Dangky = (idMon, tenMon = "") => {
+        //   var selector = document.getElementById(idMon)
+        if (document.getElementById(idMon) == null) return 0;
+        if (document.getElementById(idMon).disabled) {
+            console.log(`%cĐăng ký môn ${tenMon} (${idMon}) thất bại do hết slot!\n`, 'color: red;');
+            writeLog(`<p style="color:red;margin-top:0;margin-bottom:0;">Đăng ký môn ${tenMon} (${idMon}) thất bại do hết slot!</p>`);
+            return 1;
+        }
+
+        if (document.getElementById(idMon).checked) {
+            console.log(`%cBạn đã đăng ký ${tenMon} (${idMon}) trước đó!\n`, 'color: red;');
+            writeLog(`<p style="color:red;margin-top:0;margin-bottom:0;">Bạn đã đăng ký ${tenMon} (${idMon}) trước đó!</p>`);
+            return 1;
+        }
+
+        document.getElementById(idMon).checked = true
+        toDKSelectedChange(document.getElementById(idMon))
+        console.log(`%cĐã đăng ký ${tenMon} (${idMon}) thành công`, 'color: green;');
+        writeLog(`<p style="color:green;margin-top:0;margin-bottom:0;">Bạn đã đăng ký ${tenMon} (${idMon}) thành công!</p>`);
+        return 1;
+    }
+
+    async function Dangkymon(thongtin) {
+        await switchTo(thongtin.tableId);
+        await DelayTime(2000)
+        var failedCount = 0
         setTimeout(() => {
-            res();
-        }, sec)
-    })
-}
-
-async function RunAuto(dss) {
-    for (let i = 0; i < dss.length; i++) {
-        await DelayTime(1000)
-        Dangkymon(dss[i])
-        await DelayTime(delayTime)
+            while (Dangky(`chk_${thongtin.tableId}${thongtin.key}    `, thongtin.name) == 0 && failedCount < 10) {
+                // DelayTime()
+                failedCount++;
+                console.error('%cFailed! \n', 'color: black;');
+            }
+        }, 1000)
     }
-    $("#autopick").val("Đã xong!")
-    if (document.getElementById("autosave").checked) {
-        LuuDanhSachDangKy()
-        console.log("Tu dong luu thanh cong")
-    }
-}
 
-lamMoi()
+
+
+
+
+
+    const DelayTime = (sec) => {
+        return new Promise((res) => {
+            setTimeout(() => {
+                res();
+            }, sec)
+        })
+    }
+
+    async function RunAuto(dss) {
+        for (let i = 0; i < dss.length; i++) {
+            await DelayTime(1000)
+            Dangkymon(dss[i])
+            await DelayTime(delayTime)
+        }
+        $("#autopick").val("Đã xong!")
+        if (document.getElementById("autosave").checked) {
+            LuuDanhSachDangKy()
+            console.log("Tu dong luu thanh cong")
+        }
+    }
+
+    lamMoi()
+
+})();
